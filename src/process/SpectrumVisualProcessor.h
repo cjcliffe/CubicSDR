10bc0c8ec5aece4ef1325b1b09b0c4191ce7a908
Charles J. Cliffe <cj@cubicproductions.com>
2015-07-27 22:30:25 -0400

    spectrum + waterfall processor init

---
+++ src/process/SpectrumVisualProcessor.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "VisualProcessor.h"
+
+class SpectrumVisualProcessor : public VisualProcessor {
+protected:
+    virtual void process() {
+        
+    }
+};
---
+++ src/process/WaterfallVisualProcessor.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "VisualProcessor.h"
+
+
+class WaterfallVisualProcessor : public VisualProcessor {
+protected:
+    virtual void process() {
+        
+    }
+};
