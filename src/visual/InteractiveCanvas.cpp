7b48b4f69fa94c5d7ebdb211b1e01290f9c36d3a
Charles J. Cliffe <cj@cubicproductions.com>
2015-11-01 16:12:22 -0500

    Fix arrow key input on windows

--- src/visual/InteractiveCanvas.cpp
+++ src/visual/InteractiveCanvas.cpp
@@ -19,7 +19,7 @@
 
 InteractiveCanvas::InteractiveCanvas(wxWindow *parent, int *attribList) :
         wxGLCanvas(parent, wxID_ANY, attribList, wxDefaultPosition, wxDefaultSize,
-        wxFULL_REPAINT_ON_RESIZE), parent(parent), shiftDown(false), altDown(false), ctrlDown(false), centerFreq(0), bandwidth(0), lastBandwidth(0), isView(
+        wxFULL_REPAINT_ON_RESIZE| wxWANTS_CHARS), parent(parent), shiftDown(false), altDown(false), ctrlDown(false), centerFreq(0), bandwidth(0), lastBandwidth(0), isView(
         false) {
     mouseTracker.setTarget(this);
 }
