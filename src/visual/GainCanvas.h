3eed66938222711ec9731798234a5078ab99a8e6
Charles J. Cliffe <cj@cubicproductions.com>
2016-07-08 19:18:28 -0400

    Comment cleanup

--- src/visual/GainCanvas.h
+++ src/visual/GainCanvas.h
@@ -14,17 +14,6 @@
 #include "Timer.h"
 #include "MeterPanel.h"
 
-//class GainInfo {
-//public:
-//    std::string name;
-//    float low, high, current;
-//    bool changed;
-//    GLPanel panel;
-//    GLPanel levelPanel;
-//    GLPanel highlightPanel;
-//    GLTextPanel labelPanel;
-//    GLTextPanel valuePanel;
-//};
 
 class GainCanvas: public InteractiveCanvas {
 public:
@@ -39,7 +28,6 @@ private:
     void OnPaint(wxPaintEvent& event);
     void OnIdle(wxIdleEvent &event);
 
-//    int GetPanelHit(CubicVR::vec2 &result);
     void SetLevel();
 
     void OnShow(wxShowEvent& event);
@@ -52,7 +40,6 @@ private:
 
     PrimaryGLContext *glContext;
     std::string helpTip;
-//    std::vector<GainInfo *> gainInfo;
     std::vector<MeterPanel *> gainPanels;
     GLPanel bgPanel;
     SDRRangeMap gains;
