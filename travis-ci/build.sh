02794adb59b56fc0928bda8e409dd36ece4b433a
Charles J. Cliffe <cj@cubicproductions.com>
2019-01-07 22:07:44 -0500

    Update build.sh

--- travis-ci/build.sh
+++ travis-ci/build.sh
@@ -1,26 +1,28 @@
-cd $HOME/build
 # Liquid-DSP
+cd $HOME/build
 mkdir jgaeddert/
-git clone https://github.com/jgaeddert/liquid-dsp.git jgaeddert/liquid-dsp
-cd jgaeddert/liquid-dsp/
+cd jgaeddert/
+git clone https://github.com/jgaeddert/liquid-dsp.git
+cd liquid-dsp/
 ./bootstrap.sh
 ./configure
 make -j2
 sudo make install
 
-cd $HOME/build
 # Soapy SDR
+cd $HOME/build
 mkdir pothosware/
-git clone https://github.com/pothosware/SoapySDR.git pothosware/SoapySDR
-mkdir pothosware/SoapySDR-build
-cmake pothosware/SoapySDR pothosware/SoapySDR-build
-cd pothosware/SoapySDR-build
+cd pothosware/
+git clone https://github.com/pothosware/SoapySDR.git
+mkdir SoapySDR-build
+cd SoapySDR-build
+cmake ../SoapySDR -DCMAKE_BUILD_TYPE=Release
 make -j2
 sudo make install
 
-cd $HOME/build
 # CubicSDR
+cd $HOME/build
 mkdir cjcliffe/CubicSDR-build
-cmake cjcliffe/CubicSDR cjcliffe/CubicSDR-build
 cd cjcliffe/CubicSDR-build
+cmake ../CubicSDR -DCMAKE_BUILD_TYPE=Release
 make -j2
