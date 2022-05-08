521f62900a249916ba9432957e4bc04cf7b352f6
classabbyamp <dev@placeviolette.net>
2022-05-08 16:15:28 -0400

    fix build errors caused by commented parameters in function declarations

--- src/visual/WaterfallCanvas.cpp
+++ src/visual/WaterfallCanvas.cpp
@@ -482,7 +482,7 @@ void WaterfallCanvas::OnKeyDown(wxKeyEvent& event) {
     }
 
 }
-void WaterfallCanvas::OnIdle(wxIdleEvent & /* event */) {
+void WaterfallCanvas::OnIdle(wxIdleEvent & event) {
     processInputQueue();
     Refresh();
 }
